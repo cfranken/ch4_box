e06014fde33ae2fb4de2fe058fb9292705365a82
newt-n94 <newt.n94@gmail.com>
2019-06-07 11:10:17 -0700

    added Turner et al, 2017 case to run_tests script

--- run_tests.m
+++ run_tests.m
@@ -1,8 +1,21 @@
 % Newton Nguyen 
 % 05/14/2018
 % A script to run tests of the inversions located in DriverScript
-% Exporting results of test to test_results.mat for python plotting because pythong plots are prettier
+% Exporting results of test to test_results.mat for python plotting because pythong plots are being used in the paper 
 
+% Reference Case from AJT 2017 paper
+DriverScript
+
+case0.concentrations = out_anal;
+case0.ems = anal_soln;
+case0.nh_ch4_ems = anal_soln{1}(:,1);
+case0.sh_ch4_ems = anal_soln{1}(:,2);
+case0.nh_co_ems = anal_soln{1}(:,13);
+case0.sh_co_ems = anal_soln{1}(:,14);
+case0.nh_oh = anal_soln{1}(:,11);
+case0.sh_oh = anal_soln{1}(:,12);
+
+save('case0_test.mat')
 
 %%%1. Run the basecase
 %%% Alex's PNAS Paper 
@@ -60,3 +73,27 @@ case3.sh_oh_ems = anal_soln{1}(:,12);
 save('case3_test.mat')
 
 
+DriverScript_case4
+
+case4.concentrations = out_anal;
+case4.ems = anal_soln;
+case4.nh_ch4_ems = anal_soln{1}(:,1);
+case4.sh_ch4_ems = anal_soln{1}(:,2);
+case4.nh_co_ems = anal_soln{1}(:,13);
+case4.sh_co_ems = anal_soln{1}(:,14);
+case4.nh_oh_ems = anal_soln{1}(:,11);
+case4.sh_oh_ems = anal_soln{1}(:,12);
+
+save('case4_test.mat')
+
+DriverScript_case5
+case5.concentrations = out_anal;
+case5.ems = anal_soln;
+case5.nh_ch4_ems = anal_soln{1}(:,1);
+case5.sh_ch4_ems = anal_soln{1}(:,2);
+case5.nh_co_ems = anal_soln{1}(:,13);
+case5.sh_co_ems = anal_soln{1}(:,14);
+case5.nh_oh_ems = anal_soln{1}(:,11);
+case5.sh_oh_ems = anal_soln{1}(:,12);
+
+save('case5_test.mat')
